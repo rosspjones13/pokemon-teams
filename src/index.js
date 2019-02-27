41d892f11d42aea627012223aea68de00d8d1bf3
Ross Jones <rossmosis.jones@gmail.com>
2019-02-27 10:19:00 -0500

    step 1 and 2 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
 //solution goes here
+document.addEventListener("DOMContentLoaded", init)
+
+// Returns server url
+function getUrl() {
+  return 'http://localhost:3000/'
+}
+
+// After DOM loaded
+function init() {
+  getAllTrainers()
+}
+
+// Get trainers from server
+function getAllTrainers() {
+  fetch(getUrl() + 'trainers')
+    .then(res => res.json())
+    .then(trainers => trainers.forEach(trainer => renderTrainer(trainer)))
+}
+
+// Render each trainer
+function renderTrainer(trainer) {
+  // Grab main tag
+  let main = document.querySelector('main')
+
+  // Create div and inner elements
+  let div = document.createElement('div')
+  div.className = "card"
+  let p = document.createElement('p')
+  p.innerText = trainer.name
+  let addButton = document.createElement('button')
+  addButton.innerText = "Add Pokemon"
+  let ul = document.createElement('ul')
+
+  // Create pokemon list items to append to ul
+  trainer.pokemons.forEach(pokemon => {
+    let li = document.createElement('li')
+    li.innerText = pokemon.nickname + ` (${pokemon.species})`
+    let releaseBtn = document.createElement('button')
+    releaseBtn.className = "release"
+    releaseBtn.innerText = "Release"
+    
+    // Append button to pokemon li and then li to ul
+    li.appendChild(releaseBtn)
+    ul.appendChild(li)
+  })
+
+  // Append trainer elements to div
+  div.append(p, addButton, ul)
+
+  // Append whole trainer div to main
+  main.append(div)
+
+  // After element is visible add an event listener to add pokemon
+  addButton.addEventListener('click', () => getPokemon(trainer))
+}
+
+// Adds a pokemon to the trainer on the server
+function getPokemon(trainer) {
+  data = {trainer_id: trainer.id}
+  fetch((getUrl() + 'pokemons'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(renderTrainer(trainer))
+}
+
+function renderPokemon(trainer) {
+  
+}
+
