133aaf0afed5548f737195ec3bbd843bb580d549
Ross Jones <rossmosis.jones@gmail.com>
2019-02-27 10:47:41 -0500

    completed step 3

--- src/index.js
+++ src/index.js
@@ -31,45 +31,61 @@ function renderTrainer(trainer) {
   let addButton = document.createElement('button')
   addButton.innerText = "Add Pokemon"
   let ul = document.createElement('ul')
+  ul.dataset.trainerId = trainer.id
 
   // Create pokemon list items to append to ul
-  trainer.pokemons.forEach(pokemon => {
-    let li = document.createElement('li')
-    li.innerText = pokemon.nickname + ` (${pokemon.species})`
-    let releaseBtn = document.createElement('button')
-    releaseBtn.className = "release"
-    releaseBtn.innerText = "Release"
+  // trainer.pokemons.forEach(pokemon => {
+    //   let li = document.createElement('li')
+    //   li.innerText = pokemon.nickname + ` (${pokemon.species})`
+    //   let releaseBtn = document.createElement('button')
+    //   releaseBtn.className = "release"
+    //   releaseBtn.innerText = "Release"
     
-    // Append button to pokemon li and then li to ul
-    li.appendChild(releaseBtn)
-    ul.appendChild(li)
-  })
-
-  // Append trainer elements to div
-  div.append(p, addButton, ul)
-
-  // Append whole trainer div to main
-  main.append(div)
-
-  // After element is visible add an event listener to add pokemon
-  addButton.addEventListener('click', () => getPokemon(trainer))
-}
-
-// Adds a pokemon to the trainer on the server
-function getPokemon(trainer) {
-  data = {trainer_id: trainer.id}
-  fetch((getUrl() + 'pokemons'), {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(renderTrainer(trainer))
-}
-
-function renderPokemon(trainer) {
+    //   // Append button to pokemon li and then li to ul
+    //   li.appendChild(releaseBtn)
+    //   ul.appendChild(li)
+    // })
+    
+    // Append trainer elements to div
+    div.append(p, addButton, ul)
+    
+    // Append whole trainer div to main
+    main.append(div)
+    trainer.pokemons.forEach(pokemon => renderPokemon(pokemon))
+    
+    // After element is visible add an event listener to add pokemon
+    addButton.addEventListener('click', () => getPokemon(trainer))
+  }
   
+  // Adds a pokemon to the trainer on the server
+  function getPokemon(trainer) {
+    data = {trainer_id: trainer.id}
+    fetch((getUrl() + 'pokemons'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(pokemon => renderPokemon(pokemon))
+}
+
+// Renders a trainers single pokemon
+function renderPokemon(pokemon) {
+  // Get ul to clear current list and append new full pokemon list 
+  let ul = document.querySelector(`[data-trainer-id="${pokemon.trainer_id}"]`)
+  // debugger
+
+  // Grab all of trainers pokemon
+  let li = document.createElement('li')
+  li.innerText = pokemon.nickname + ` (${pokemon.species})`
+  let releaseBtn = document.createElement('button')
+  releaseBtn.className = "release"
+  releaseBtn.innerText = "Release"
+
+  // Append button to pokemon li and then li to ul
+  li.appendChild(releaseBtn)
+  ul.appendChild(li)
 }
 
